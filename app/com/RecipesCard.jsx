bb41492600f2658692d14e280b3031df8191b169
mehdi0333 <164217277+benabdallahSamir@users.noreply.github.com>
2025-07-28 19:40:29 +0100

    fix busg and end all recipe card information

--- app/com/RecipesCard.jsx
+++ app/com/RecipesCard.jsx
@@ -1,21 +1,59 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 const RecipesCard = ({ meal }) => {
-  console.log(meal);
+  const [loading, setLoading] = useState(true);
+  const [mealDetails, setMealDetails] = useState(null);
+  const router = useRouter();
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await fetch(
+          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`
+        );
+        const data = await response.json();
+        if (data.meals) {
+          meal = data.meals[0];
+          setMealDetails(meal);
+        }
+      } catch (error) {
+        console.error("Error fetching meal data:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (loading) return null; // or a loading spinner
   return (
     <View
       style={tw`py-[15] px-[5] bg-gray-100 overflow-hidden w-[49%] rounded-lg my-[10]`}
     >
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => router.push(`/meal/${mealDetails.idMeal}`)}
+      >
         <Image
-          source={{ uri: meal?.strMealThumb }}
+          source={{ uri: mealDetails?.strMealThumb }}
           style={tw`h-[40] w-full rounded-lg`}
         />
-        <Text style={tw`text-blue-700 font-semibold mt-[5] text-2xl`}>
-          {meal?.strMeal.substring(0, 20) +
-            (meal?.strMeal.length > 20 ? "..." : "")}
+        <Text
+          style={tw`text-blue-700 font-semibold mt-[5] text-2xl`}
+          numberOfLines={2}
+        >
+          {mealDetails?.strMeal}
         </Text>
+        <Text style={tw`text-xl mt-[5]`} numberOfLines={3}>
+          {mealDetails?.strInstructions}
+        </Text>
+        <View style={tw`flex-row gap-2 items-center`}>
+          <Ionicons name="location-outline" size={16} color="gray" />
+          <Text style={tw`text-gray-500 text-sm`}>
+            {mealDetails?.strArea || "Unknown"}
+          </Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
--- app/com/recipes/RandomMeal.jsx
+++ app/com/recipes/RandomMeal.jsx
@@ -1,10 +1,16 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 const RandomMeal = ({ meal }) => {
+  const router = useRouter();
   return (
     <View style={tw`flex-row justify-around items-center`}>
-      <TouchableOpacity style={tw`w-full relative`}>
+      <TouchableOpacity
+        style={tw`w-full relative`}
+        onPress={() => router.push(`/meal/${meal?.idMeal}`)}
+      >
         <Image
           source={{
             uri: meal?.strMealThumb,
@@ -12,11 +18,21 @@ const RandomMeal = ({ meal }) => {
           style={tw`w-full object-cover h-[60] rounded-[5]`}
         />
       </TouchableOpacity>
-      <Text
-        style={tw`text-center text-sm block bg-gray-300/50 px-[3] rounded-md font-semibold mt-[5] text-2xl absolute bottom-[10] left-[5] text-white font-bold`}
-      >
-        {meal?.strMeal}
-      </Text>
+      <View style={tw`absolute bottom-[5] left-[5] flex-col`}>
+        <Text
+          style={tw`text-center text-sm block rounded-md font-semibold mt-[5] text-2xl text-white font-bold`}
+        >
+          {meal?.strMeal}
+        </Text>
+        <View style={tw`flex-row items-center gap-[2]`}>
+          <Ionicons name="location-outline" size={24} color="white" />
+          <Text
+            style={tw`text-center text-sm block rounded-md font-semibold text-base text-white`}
+          >
+            {meal?.strArea || "Unknown"}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };

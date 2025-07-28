bf52bad0e36e0fdfa29dfd6ec28a865793ad8d92
mehdi0333 <164217277+benabdallahSamir@users.noreply.github.com>
2025-07-28 19:40:41 +0100

    add recipes page route

---
+++ app/meal/[id].jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+const MealDetail = () => {
+  // This component will display the details of a meal
+  // You can fetch the meal details using the meal ID from the route params
+  // and display them here.a
+  // For now, we will just return a placeholder view
+  // You can use the useRouter hook to get the meal ID from the URL
+  return (
+    <View>
+      <Text>Meal Detail</Text>
+    </View>
+  );
+};
+
+export default MealDetail;
